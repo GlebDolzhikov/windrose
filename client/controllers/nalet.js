df9888b6a3835310c7c1d6f664e70f296ae32eef
gleb <9276677@gmail.com>
2016-02-19 17:37:53 +0200

    month counter

--- client/controllers/nalet.js
+++ client/controllers/nalet.js
@@ -2,6 +2,11 @@ Template.nalet.helpers({
     ttlDuration: function(){
         var skedObj =  Data.findOne(Template.parentData(2)._id);
         var ttlTime = 0;
+
+        var monthsTimeCounter = [];
+        for(var i = 0; i<13;i++){
+            monthsTimeCounter[i] = 0;
+        }
         Data.find({
             name:'flight',
             bort: this.createdAt,
@@ -13,19 +18,35 @@ Template.nalet.helpers({
                 var skedRange = moment.range(new Date(skedObj.range[0]),new Date(skedObj.range[1]));
                 var fltRange =   moment.range(new Date(el.range[0]),new Date(el.range[1]));
                 var counter = 0;
+                var monthsCounter = [];
+                for(var i = 0; i<13;i++){
+                    monthsCounter[i] = 0;
+                }
                 if(skedRange.overlaps(fltRange)){
                     fltRange.by('days', function(moment) {
                         if (skedRange.contains(moment)&&moment.weekday() == el.dayNum) {
-                            counter++;
+                            monthsCounter[moment.month()+1]++; //счетчик месяцев
+                            counter++; // общий счетчик
                         }
                     });
                 }
+                for(var i = 0; i<12;i++){
+                    if(monthsCounter[i] != 0) {
+                        monthsTimeCounter[i] += ((el.width/3)*5)*monthsCounter[i]
+                    }
+                }
+
                 ttlTime += (el.width/3)*5*counter;
 
             }else{
                 ttlTime += ((el.width/3)*5)*4.3;
             }
         });
-        return Math.round(ttlTime/60);
+        //возвращаем объект + массив с налетами за месяца
+        var x;
+        for (x in monthsTimeCounter){
+                monthsTimeCounter[x] = Math.round(monthsTimeCounter[x]/60)
+        }
+        return {ttl:Math.round(ttlTime/60),month:monthsTimeCounter}
     }
 });
