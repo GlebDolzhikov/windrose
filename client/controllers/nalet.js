0416708a94ebb22afb3f3b55af02b380ad4f29c7
gleb <9276677@gmail.com>
2016-02-24 16:32:38 +0200

    Bort name update

--- client/controllers/nalet.js
+++ client/controllers/nalet.js
@@ -24,42 +24,45 @@ Template.nalet.helpers({
             bort: this.createdAt,
             skedId: Template.parentData(2)._id
         }).forEach(function(el){
-            if(el.range&&skedObj.range) {
-                if (skedObj.range[0].length > 1 && el.range[0]) {
-                    var skedRange = moment.range(dateFormatSlash(skedObj.range[0]),dateFormatSlash(skedObj.range[1]));
-                    var fltRange = moment.range(dateFormat(el.range[0]), dateFormat(el.range[1]));
-                    var counter = 0;
-                    var monthsCounter = [];
-                    for (var i = 0; i < 13; i++) {
-                        monthsCounter[i] = 0;
-                    }
-                    if(el.singleDays){ //если период не задан, а заданы единичные даты
-                        for(var i = 0;i < el.range.length; i++){
-                            var fltDay = moment(el.range[i], "DD-MM-YYYY");
-                            monthsCounter[fltDay.month() + 1]++;
-                            if(skedRange.contains(fltDay)){
-                                counter++
+            if(skedObj.range){
+                if(el.range) {
+                    if (skedObj.range[0].length > 1 && el.range[0]) {
+                        var skedRange = moment.range(dateFormatSlash(skedObj.range[0]),dateFormatSlash(skedObj.range[1]));
+                        var fltRange = moment.range(dateFormat(el.range[0]), dateFormat(el.range[1]));
+                        var counter = 0;
+                        var monthsCounter = [];
+                        for (var i = 0; i < 13; i++) {
+                            monthsCounter[i] = 0;
+                        }
+                        if(el.singleDays){ //если период не задан, а заданы единичные даты
+                            for(var i = 0;i < el.range.length; i++){
+                                var fltDay = moment(el.range[i], "DD-MM-YYYY");
+                                monthsCounter[fltDay.month() + 1]++;
+                                if(skedRange.contains(fltDay)){
+                                    counter++
+                                }
+                            }
+                        }else {
+                            if (skedRange.overlaps(fltRange)) { //если период задан
+                                fltRange.by('days', function (moment) {
+                                    if (moment.weekday() == el.dayNum) {
+                                        monthsCounter[moment.month() + 1]++; //счетчик месяцев
+                                        if(skedRange.contains(moment)){
+                                            counter++; // общий счетчик
+
+                                        }
+                                    }
+                                });
                             }
                         }
-                    }else {
-                        if (skedRange.overlaps(fltRange)) { //если период задан
-                            fltRange.by('days', function (moment) {
-                                if (moment.weekday() == el.dayNum) {
-                                    monthsCounter[moment.month() + 1]++; //счетчик месяцев
-                                    if(skedRange.contains(moment)){
-                                        counter++; // общий счетчик
-
-                                    }
-                                }
-                            });
+                        for (var i = 0; i < 12; i++) {
+                            if (monthsCounter[i] != 0) {
+                                monthsTimeCounter[i] += ((el.width / 3) * 5) * monthsCounter[i]
+                            }
                         }
+                        ttlTime += (el.width / 3) * 5 * counter;
+                    }else{
                     }
-                    for (var i = 0; i < 12; i++) {
-                        if (monthsCounter[i] != 0) {
-                            monthsTimeCounter[i] += ((el.width / 3) * 5) * monthsCounter[i]
-                        }
-                    }
-                    ttlTime += (el.width / 3) * 5 * counter;
                 }
             }else {
                 ttlTime += ((el.width/3)*5)*4.3;
@@ -75,7 +78,10 @@ Template.nalet.helpers({
         return {ttl:Math.round(ttlTime/60),month:monthsTimeCounter}
     },
     range:function(){
-        console.log( Template.parentData(1).range)
         return Template.parentData(1).range
+    },
+    months:function(index){
+        arr = ["01","02","03","04","05","06","07","08","09","10","11","12"];
+        return arr[index-1]
     }
 });
